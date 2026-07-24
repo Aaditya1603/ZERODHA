c3525aa420bcbaf35d34617059cd578dc5c2c31d
Aaditya Bhardwaj <bharwajaaditya@gmail.com>
2026-07-24 17:50:50 +0530

    update code

--- frontend/src/landing_page/about/Team.js
+++ frontend/src/landing_page/about/Team.js
@@ -25,10 +25,9 @@ function Team() {
         </div>
         <div className="col-6 p-3">
           <p>
-            Nithin bootstrapped and founded Zerodha in 2010 to overcome the
-            hurdles he faced during his decade long stint as a trader. Today,
-            Zerodha has changed the landscape of the Indian broking
-            industry.{" "}
+            Aaditya founded Zerodha in 2010 to overcome the hurdles he faced
+            during his decade long stint as a trader. Today, Zerodha has changed
+            the landscape of the Indian broking industry.{" "}
           </p>
           <p>
             He is a member of the SEBI Secondary Market Advisory Committee
