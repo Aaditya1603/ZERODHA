21b47b785af37a3a712e45da0645bfce4e07b2c3
Aaditya Bhardwaj <bharwajaaditya@gmail.com>
2026-08-01 00:47:13 +0530

    adding dark mode functionality to  dashboard

--- dashboard/src/index.js
+++ dashboard/src/index.js
@@ -1,15 +1,24 @@
-import React from "react";
+import React, { useEffect } from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./index.css";
 import Home from "./components/Home";
+
+function ThemeWrapper() {
+  useEffect(() => {
+    const savedTheme = localStorage.getItem("app-theme") || "light";
+    document.documentElement.setAttribute("data-theme", savedTheme);
+  }, []);
+
+  return <Home />;
+}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/*" element={<Home />} />
+        <Route path="/*" element={<ThemeWrapper />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>,
