332e2a9ad48385b50e099713367f631c6b51b6ff
Aaditya Bhardwaj <bharwajaaditya@gmail.com>
2026-08-03 18:21:50 +0530

    fixing issue on buy window and sell window on light mode readability

--- dashboard/src/components/BuyActionWindow.js
+++ dashboard/src/components/BuyActionWindow.js
@@ -33,7 +33,7 @@ const BuyActionWindow = ({ uid }) => {
   };
 
   return (
-    <div className="buy-action-modal-card" id="buy-window" draggable="true">
+    <div className="container" id="buy-window" draggable="true">
       <div className="regular-order">
         <div className="inputs">
           <fieldset>
